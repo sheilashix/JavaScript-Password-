8c3b315418b218ce8e2bee9d6efa63596dfc3d2b
sheilashix <70675711+sheilashix@users.noreply.github.com>
2021-02-21 21:32:36 -0700

    I have changed some small coding
    I have changed some small Script.js coding

--- script.js
+++ script.js
@@ -25,6 +25,8 @@ function generatePassward() {
   // taking the lenghth of the password.
  
     var lenghth = window.prompt("what is the length of the Password should be >8 and <128");
+    lenghth = lenghth + 2
+
 
 // taking user preffrences
     
@@ -32,6 +34,7 @@ function generatePassward() {
      var  small = window.prompt("Number of lowercase letters to be included");
      var numbers  = window.prompt("Number of numbers  to be included");
      var speicial  = window.prompt("Number of special charaters letters to be included");
+  
 // total character 
 var total = (capitals+ small + number+special);
 
@@ -40,29 +43,29 @@ var total = (capitals+ small + number+special);
  var  Capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
  var   Small =  "abcdefghijklmnaogrtstuvwxwz";
  var   Numbers ="1234567890";
- var   Special = "!@$%&*?";
+ var   Special = "!@$%&*?#";
 
 
 // creating loops for each choice
 var CAPITALS ="";
 
-for(var i = 0; i <= capitals; i++){
+for(var i = 0; i < capitals; i++){
     CAPITALS = CAPITALS + Capitals.charAt(Math.floor(Math.random() * Math.floor(Capitals.length - 1)));
 }
 //window.alert(CAPITALS);
 var SMALL ="";
-for(var i = 0; i <= small; i++){
+for(var i = 0; i < small; i++){
     SMALL= SMALL + Small.charAt(Math.floor(Math.random() * Math.floor(Small.length - 1)));
 }
 //window.alert(SMALL);
 var NUMBERS ="";
 for(var i = 0; i <= number; i++){
-    NUMBERS= NUMBERS+ Numbers.charAt(Math.floor(Math.random() * Math.floor(Numbers.length - 1)));
+    NUMBERS= NUMBERS+ Numbers.charAt(Math.floor(Math.random() * Math.floor(Numbers.length +1)));
 }
 //window.alert(NUMBERS)
 var SPECIAL= "";
 for(var i = 0; i <= special; i++){
-    SPECIAL= SPECIAL+ Special.charAt(Math.floor(Math.random() * Math.floor(Special.length - 1)));
+    SPECIAL= SPECIAL+ Special.charAt(Math.floor(Math.random() * Math.floor(Special.length +1)));
 }
 //window.alert(SPECIAL);
 //joinning the password
